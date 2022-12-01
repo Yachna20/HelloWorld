8634899b7a60f9e2b37e4a6a4e0053605cf4c2ce
Yachna20 <thakreyachna20@gmail.com>
2022-12-01 10:22:12 +0530

    [Yachna]-It is a hello world

---
+++ HelloWorld!!/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+    }
+}
+
